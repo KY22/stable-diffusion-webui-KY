47510232ffedc30b37d0c6194f794308e2660c6e
L. Martínez <fatulazy@gmail.com>
2024-01-03 13:48:13 -0300

    EDIT

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS= --xformers --no-download-sd-model --ckpt-dir "D:\SD\Data\Models\StableDiffusion" --vae-dir "D:\SD\Data\Models\VAE" --lora-dir "D:\SD\Data\Models\Lora" --hypernetwork-dir "D:\SD\Data\Models\Hypernetwork"
 
 call webui.bat
